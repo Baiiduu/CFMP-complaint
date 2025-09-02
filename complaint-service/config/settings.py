257dc92ff0c45a7897c0920679a2f3bb24d5f1b1
Baidu <2698425919@qq.com>
2025-09-02 09:26:16 +0800

    ComplaintView建权

--- complaint-service/config/settings.py
+++ complaint-service/config/settings.py
@@ -105,9 +105,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # REST Framework settings
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
-    ],
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend',
         'rest_framework.filters.OrderingFilter',
